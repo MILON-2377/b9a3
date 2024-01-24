9915a952decd653efed64a67743071d7e2fd6cfe
MILON-2377 <153436745+MILON-2377@users.noreply.github.com>
2024-01-24 13:56:23 +0800

    question and answer section commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,6 @@ module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
       extend: {
-         backgroundImage: {
-            'banner-img1': "url('/images/banner1.png')",
-            'banner-img2': "url('/banner2.png')",
-            'banner-img3': "url('/banner3.png')",
-         },
       },
     },
     plugins: [],
