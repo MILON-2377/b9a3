26daf0b02cf8fb04f4bb4dab1713a3f91404ccdc
MILON-2377 <153436745+MILON-2377@users.noreply.github.com>
2024-01-25 15:58:37 +0800

    final commit for all section

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
       extend: {
+        backgroundImage: {
+          'banner-img1': "url('/images/1.png')",
+        },
       },
     },
     plugins: [],
